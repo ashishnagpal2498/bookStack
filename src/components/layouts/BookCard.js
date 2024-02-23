d0a66f48bf72fb8dbe6bf1f4085068bdbf344e83
unknown <ashishnagpal2498@gmail.com>
2024-02-22 21:17:26 -0400

    FilterBtn component separated

--- src/components/layouts/BookCard.js
+++ src/components/layouts/BookCard.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import bookUrl1 from "../../book-cover-1.png"
 import bookUrl2 from "../../book-cover-2.jpg"
-const BookCard = ({books}) => {
+const BookCard = ({ books, loading }) => {
   return (
     <>
-    {books.map((book, index) => (
+      {books.map((book, index) => (
         <li className="book-card" key={index}>
-          <img className="book-cover" src={book.title.includes("Potter") ? bookUrl2 : bookUrl1} alt={book.title} />
+          
+          {loading ?
+              <div className="skeleton-book-bg">
+                <div className='skeleton-book-cover'>
+                </div>
+              </div>
+            : 
+            <div className='book-cover'>
+            <img
+              className="book-img"
+              src={book.title.includes("Potter") ? bookUrl2 : bookUrl1}
+              alt={book.title}
+            />
+            </div>
+            }
           <div className="book-shelf"></div>
         </li>
       ))}
